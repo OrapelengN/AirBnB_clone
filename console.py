ac7d6aec6c40589f78edc4058a7332540f772b5c
OrapelengN <portiaoran@gmail.com>
2024-11-19 22:45:06 -0800

    6. Console 0.0.1

--- console.py
+++ console.py
@@ -13,21 +13,12 @@ class HBNBCommand(cmd.Cmd):
 
     def do_EOF(self, arg):
         """EOF command to exit the program"""
-        print()
+        print()  # Print a newline for clean output
         return True
 
     def emptyline(self):
         """Do nothing on empty input line"""
         pass
-
-    def do_create(self, arg):
-        """Create a new instance of BaseModel"""
-        if not arg:
-            print("** class name missing **")
-        elif arg == "BaseModel":
-            new_instance = BaseModel()
-            new_instance.save()
-            print(new_instance.id)
 
 
 if __name__ == "__main__":
